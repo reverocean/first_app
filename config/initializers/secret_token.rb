128ee610e42d2a384bdd4fa1122d1807e9bae7cb
reverocean <reverocean@gmail.com>
2012-04-17 22:06:02 +0800

    first_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '90a331c814f3d55f4496c0923acfc0faeb30a485da9e0ec2f69893a07de60ff395b9c81781e1800ee8fb5e1facddb45dcd089ab51cf538b83035c7c8cc2a8fe0'
